c9f6a19293d407dccf89fcbf1242242faeb66e55
Bailey Simrell <bsimrell@baileySimrell.local>
2020-12-15 22:22:52 -0500

    Added UTM parser

---
+++ save-utm-parameters/main.js
@@ -0,0 +1,25 @@
+// this script saves URL parameters and then inserts them into any form that has a name matching the parameter key
+// mainly intended for hidden fields
+// (if embedding in-line, place this snippet before the closing </body> inside of <script> </script> tags)
+
+var queryForm = function(settings){
+    var reset = settings && settings.reset ? settings.reset : false;
+    var self = window.location.toString();
+    var querystring = self.split("?");
+    if (querystring.length > 1) {
+      var pairs = querystring[1].split("&");
+      for (i in pairs) {
+        var keyval = pairs[i].split("=");
+        if (reset || sessionStorage.getItem(keyval[0]) === null) {
+          sessionStorage.setItem(keyval[0], decodeURIComponent(keyval[1]));
+        }
+      }
+    }
+    var hiddenFields = document.querySelectorAll("input[type=hidden], input[type=text]");
+    for (var i=0; i<hiddenFields.length; i++) {
+      var param = sessionStorage.getItem(hiddenFields[i].name);
+      if (param) document.getElementsByName(hiddenFields[i].name)[0].value = param;
+    }
+}
+
+setTimeout(function(){ queryForm(); }, 3000);
